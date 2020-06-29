57f11dd076746e4129e4bc252735c07147ec0f50
Code Hedgehog <codehedgehog@users.noreply.github.com>
2020-06-28 23:51:37 -0700

    Update to .NET Core 3.1 and update .NuGet packages

--- src/Csuf.WebApplTemplate.MvcClientApp/Program.cs
+++ src/Csuf.WebApplTemplate.MvcClientApp/Program.cs
@@ -1,47 +1,49 @@
 ﻿// (c) California State University, Fullerton. All rights reserved.
 
-namespace Csuf.WebApplTemplate
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Enrichers.AspnetcoreHttpcontext;
+using Serilog.Exceptions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Csuf.WebApplTemplate.MvcClientApp
 {
-
-	using Microsoft.AspNetCore;
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.Logging;
-	using Serilog;
-	using Serilog.Exceptions;
-	using System;
-	using System.IO;
-
 	public class Program
 	{
-
-		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-			.SetBasePath(Directory.GetCurrentDirectory())
-			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-			.AddEnvironmentVariables()
-			.Build();
+		public static IConfiguration Configuration { get; set; }
 
 		private static string _environmentName;
 
-		public static void Main(string[] args)
+		public static int Main(string[] args)
 		{
-			Log.Logger = new LoggerConfiguration()
-				.Enrich.FromLogContext()
-				.Enrich.WithMachineName()
-				.Enrich.WithThreadId()
-				.Enrich.WithExceptionDetails()
-				.ReadFrom.Configuration(Configuration)
-				.WriteTo.Console()
-				.CreateLogger();
+			Serilog.Debugging.SelfLog.Enable(msg =>
+			{
+				string devEnvironmentVariable = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+				var isDevelopment = string.IsNullOrWhiteSpace(devEnvironmentVariable) || devEnvironmentVariable.Equals("Development", StringComparison.OrdinalIgnoreCase);
+				if (isDevelopment)
+				{
+					Console.WriteLine(msg);
+					Debug.Print(msg);
+					Debugger.Break();
+				}
+			});
 			try
 			{
-				Log.Information("Starting web host");
-				CreateWebHostBuilder(args).Build().Run();
+				CreateHostBuilder(args).Build().Run();
+				return 0;
 			}
 			catch (Exception ex)
 			{
 				Log.Fatal(ex, "Host terminated unexpectedly");
+				Console.WriteLine("Host terminated unexpectedly");
+				Console.Write(ex.ToString());
+				return 1;
 			}
 			finally
 			{
@@ -49,20 +51,43 @@ namespace Csuf.WebApplTemplate
 			}
 		}
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+		.ConfigureWebHostDefaults(webBuilder =>
 		{
-			return WebHost.CreateDefaultBuilder(args)
-				 .ConfigureLogging((hostingContext, config) =>
-				 {
-					 config.ClearProviders();
-					 _environmentName = hostingContext.HostingEnvironment.EnvironmentName;
-				 })
-				 .UseKestrel(c => c.AddServerHeader = false)
-				 .UseStartup<Startup>()
-				 .UseConfiguration(Configuration)
-				 .UseSerilog();
-			;
-		}
-
+			webBuilder.UseKestrel(c => c.AddServerHeader = false);
+			webBuilder.UseStartup<Startup>();
+		})
+		.ConfigureHostConfiguration(builder =>
+		{
+			string devEnvironmentVariable = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			var isDevelopment = string.IsNullOrWhiteSpace(devEnvironmentVariable) || devEnvironmentVariable.ToLower() == "development";
+			if (isDevelopment) builder.AddUserSecrets<Program>();
+			builder.SetBasePath(Directory.GetCurrentDirectory())  // AppContext.BaseDirectory
+				.AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+				.AddEnvironmentVariables()
+				.Build();
+			Configuration = builder.Build();
+			if (builder == null)
+			{
+				throw new Exception("Missing or invalid appsettings.json file. Please see README.md for configuration instructions.");
+			}
+		})
+		.ConfigureLogging((hostingContext, config) =>
+		{
+			config.ClearProviders();
+			_environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+		})
+		.UseSerilog((hostingContext, loggerConfiguration) =>
+		{
+			var name = Assembly.GetExecutingAssembly().GetName();
+			loggerConfiguration
+				.Enrich.FromLogContext()
+				.Enrich.WithExceptionDetails()
+				.Enrich.WithProperty("Assembly", $"{name.Name}")
+				.Enrich.WithProperty("Version", $"{name.Version}")
+				.Enrich.WithMachineName()
+				.Enrich.WithThreadId()
+				.ReadFrom.Configuration(Configuration);
+		});
 	}
 }
--- src/Csuf.WebApplTemplate.MvcClientApp/Startup.cs
+++ src/Csuf.WebApplTemplate.MvcClientApp/Startup.cs
@@ -1,73 +1,84 @@
 ﻿// (c) California State University, Fullerton. All rights reserved.
 
-namespace Csuf.WebApplTemplate
+using Csuf.WebApplTemplate.MvcClientApp.Filters;
+using Csuf.WebApplTemplate.MvcClientApp.Helpers;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static System.FormattableString;
+using static System.Globalization.CultureInfo;
+
+namespace Csuf.WebApplTemplate.MvcClientApp
 {
-	using IdentityModel;
-	using Microsoft.AspNetCore.Authentication.Cookies;
-	using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-	using Microsoft.AspNetCore.Authorization;
-	using Microsoft.AspNetCore.Builder;
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.AspNetCore.Http;
-	using Microsoft.AspNetCore.Http.Features;
-	using Microsoft.AspNetCore.Mvc;
-	using Microsoft.AspNetCore.Mvc.Authorization;
-	using Microsoft.AspNetCore.Mvc.Razor;
-	using Microsoft.AspNetCore.Routing;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-	using Microsoft.IdentityModel.Tokens;
-	using System;
-	using System.IdentityModel.Tokens.Jwt;
-	using System.Threading.Tasks;
-	using static System.FormattableString;
-	using static System.Globalization.CultureInfo;
-
-
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
-
 		public void ConfigureServices(IServiceCollection services)
 		{
-
 			services.AddRouting(options => { options.LowercaseUrls = true; });
 			services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 			services.Configure<CookiePolicyOptions>(options =>
 			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.HttpOnly = HttpOnlyPolicy.Always;
+				options.Secure = CookieSecurePolicy.Always;
 				options.CheckConsentNeeded = context => false;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
+			services.AddMvcCore(options =>
+			{
+				options.Filters.Add<SerilogLoggingPageFilter>();
+			})
+			.AddRazorPages()
+			.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+			.SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
 			services.AddMvc(options =>
 			{
+				options.Filters.Add<SerilogLoggingActionFilter>();
 				AuthorizationPolicy policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 				options.Filters.Add(new AuthorizeFilter(policy));
 				options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
 			})
-				.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
-				.AddDataAnnotationsLocalization().AddRazorOptions(options => { })
-				.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			.AddDataAnnotationsLocalization().AddRazorOptions(options => { });
 
 			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			// Put your services.AddHttpClient<>() here...
 
 			JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 			services.AddAuthentication(configureOptions: options =>
-			{
-				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = "oidc";
-				options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-			})
+																									 {
+																										 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+																										 options.DefaultChallengeScheme = "oidc";
+																										 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+																									 })
 							.AddCookie(authenticationScheme: CookieAuthenticationDefaults.AuthenticationScheme,
 												 configureOptions: options =>
 												 {
@@ -77,10 +88,10 @@ namespace Csuf.WebApplTemplate
 														 Name = Invariant($"CSUFWEBAPPLTEMPLATE{CookieAuthenticationDefaults.CookiePrefix}{CookieAuthenticationDefaults.AuthenticationScheme}"),
 														 HttpOnly = true,
 														 SecurePolicy = CookieSecurePolicy.Always,
-														 SameSite = SameSiteMode.Lax,
-														 Expiration = TimeSpan.FromMinutes(Convert.ToInt32(Configuration["CsufWebApplTemplate:CookieAuthenticationExpireTimeSpanMinutes"], InvariantCulture)),
+														 SameSite = SameSiteMode.Lax
 													 };
 													 options.SlidingExpiration = false;
+													 options.ExpireTimeSpan = TimeSpan.FromMinutes(Convert.ToInt32(Configuration["CsufWebApplTemplate:CookieAuthenticationExpireTimeSpanMinutes"], InvariantCulture));
 												 })
 							.AddOpenIdConnect(authenticationScheme: "oidc", configureOptions: options =>
 							{
@@ -91,7 +102,6 @@ namespace Csuf.WebApplTemplate
 								options.RequireHttpsMetadata = true;
 								options.TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = false, NameClaimType = JwtClaimTypes.Name, RoleClaimType = JwtClaimTypes.Role };
 								options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-
 								options.SaveTokens = true;
 								options.GetClaimsFromUserInfoEndpoint = true;
 								options.ProtocolValidator = new OpenIdConnectProtocolValidator()
@@ -113,63 +123,123 @@ namespace Csuf.WebApplTemplate
 										}
 										return Task.FromResult(0);
 									},
+									OnAuthorizationCodeReceived = (context) =>
+									{
+										Log.Debug("OnAuthorizationReceived");
+										return Task.FromResult(0);
+									},
+									OnMessageReceived = (context) =>
+									{
+										Log.Debug("OnMessageReceived");
+										return Task.FromResult(0);
+									},
+									OnRedirectToIdentityProvider = (context) =>
+									{
+										Log.Debug("OnRedirectToIdentityProvider");
+										return Task.FromResult(0);
+									},
+									OnRedirectToIdentityProviderForSignOut = (context) =>
+									{
+										Log.Debug("OnRedirectToIdentityProviderForSignOut");
+										var idTokenHint = context.HttpContext.GetTokenAsync("id_token").Result;
+										if (idTokenHint != null)
+										{
+											Log.Debug($"idTokenHint = {idTokenHint}");
+											context.ProtocolMessage.IdTokenHint = idTokenHint;
+										}
+										return Task.FromResult(0);
+									},
+									OnRemoteFailure = (context) =>
+									{
+										Log.Debug($"OnRemoteFailure {context.Failure.Message}");
+										if (context.Failure.Message.Contains("Correlation failed", StringComparison.OrdinalIgnoreCase))
+										{
+											context.HandleResponse();
+											string originalRequestedUrl = context.HttpContext.Features.Get<IHttpRequestFeature>().RawTarget;
+											context.Response.Redirect(originalRequestedUrl.Replace(@"/signin-oidc", @"/home", StringComparison.OrdinalIgnoreCase));
+										}
+										return Task.FromResult(0);
+									},
+									OnRemoteSignOut = (context) =>
+									{
+										Log.Debug("OnRemoteSignOut");
+										return Task.FromResult(0);
+									},
+									OnSignedOutCallbackRedirect = (context) =>
+									{
+										Log.Debug("OnSignedOutCallbackRedirect");
+										return Task.FromResult(0);
+									},
+									OnTicketReceived = (context) =>
+									{
+										Log.Debug("OnTicketReceived");
+										return Task.FromResult(0);
+									},
+									OnTokenResponseReceived = (context) =>
+									{
+										Log.Debug("OnTokenResponseReceived");
+										return Task.FromResult(0);
+									},
+									OnTokenValidated = (context) =>
+									{
+										Log.Debug("OnTokenValidated");
+										var nid = new ClaimsIdentity(context.Principal.Identity.AuthenticationType, JwtClaimTypes.Name, JwtClaimTypes.Role);
+										context.Principal.Claims.ToList().ForEach(x => nid.AddClaim(x));
+										// Do your Claim Transformation here..
+										context.Principal = new ClaimsPrincipal(identity: nid);
+										return Task.FromResult(0);
+									},
+									OnUserInformationReceived = (context) =>
+									{
+										Log.Debug("OnUserInformationReceived");
+										return Task.FromResult(0);
+									},
 								};
 							});
-
+			services.AddHealthChecks();
 		}
 
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app)
 		{
 			app.Use(async (context, next) =>
 			{
 				if (context.Request.Scheme.Equals("http", StringComparison.OrdinalIgnoreCase)) context.Request.Scheme = "https";
 				await next.Invoke();
 			});
+			Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
 
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				app.UseHsts();
-			}
-
-			app.UseHttpsRedirection();
-			app.UseAuthentication();
-
-			app.UseStaticFiles();
-			app.UseCookiePolicy();
+			app.UseStatusCodePagesWithReExecute(pathFormat: "/home/error", queryFormat: "?statusCode={0}");
+			app.UseExceptionHandler("/home/error");
 
 			app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
+			app.UseHttpsRedirection();
+
 			app.UseXContentTypeOptions();
 			app.UseReferrerPolicy(opts => opts.NoReferrer());
 			app.UseXXssProtection(options => options.EnabledWithBlockMode());
 			app.UseXfo(options => options.Deny());
-
-			app.UseStaticFiles();
 			app.UseNoCacheHttpHeaders();
 			app.UseXRobotsTag(options => options.NoIndex().NoFollow());
 
-			app.UseCookiePolicy(new CookiePolicyOptions()
+			app.UseStaticFiles();
+			app.UseSerilogRequestLogging(opts =>
 			{
-				MinimumSameSitePolicy = SameSiteMode.None
+				opts.EnrichDiagnosticContext = LogHelper.EnrichFromRequest;
+				opts.GetLevel = LogHelper.ExcludeHealthChecks;
 			});
+			app.UseRouting();
+			app.UseCookiePolicy(new CookiePolicyOptions() { MinimumSameSitePolicy = SameSiteMode.None });
 
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-			app.UseMvc(routes =>
+			app.UseEndpoints(endpoints =>
 			{
-				routes.MapRoute(
-									name: "default",
-									template: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapHealthChecks("/healthz");
+				endpoints.MapControllers();
+				endpoints.MapDefaultControllerRoute();
+				endpoints.MapControllerRoute(name: "DefaultRoute", pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
-
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(name: "DefaultRoute", template: "{controller=Home}/{action=Index}/{id?}");
-			});
-
 		}
 	}
 }
