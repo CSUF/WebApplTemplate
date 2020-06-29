903a7de45556b9b83f56c260ed8a334c81d8ea88
Code Hedgehog <codehedgehog@users.noreply.github.com>
2020-06-29 00:10:07 -0700

    Add error handler

--- src/Csuf.WebApplTemplate.MvcClientApp/Controllers/HomeController.cs
+++ src/Csuf.WebApplTemplate.MvcClientApp/Controllers/HomeController.cs
@@ -1,40 +1,52 @@
 ﻿// (c) California State University, Fullerton. All rights reserved.
 
+using Csuf.WebApplTemplate.MvcClientApp.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Serilog;
+using System.Diagnostics;
+using System.Text.Json;
 
-namespace Csuf.WebApplTemplate.Controllers
+namespace Csuf.WebApplTemplate.MvcClientApp.Controllers
 {
-	using Csuf.WebApplTemplate.Models;
-	using Microsoft.AspNetCore.Mvc;
-	using System.Diagnostics;
-
 	public class HomeController : Controller
 	{
 		public IActionResult Index()
 		{
-			return View();
-		}
-
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
-			return View();
-		}
-
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
+			//throw new InvalidOperationException("This is a test exception");
 			return View();
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
+		public IActionResult Error(int? statusCode = null)
 		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			ErrorViewModel modelResult = new ErrorViewModel() { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+			if (HttpContext.Features.Get<IStatusCodeReExecuteFeature>() is StatusCodeReExecuteFeature reExecuteFeature)
+			{
+				modelResult.OriginalPath = reExecuteFeature?.OriginalPath;
+				modelResult.OriginalPathBase = reExecuteFeature?.OriginalPathBase;
+				modelResult.OriginalQueryString = reExecuteFeature?.OriginalQueryString;
+				Log.Warning("Status Code: {statusCode} from request {OriginalPathBase}{OriginalPath}{OriginalQueryString} Request ID: {RequestId}",
+					statusCode, modelResult.OriginalPathBase, modelResult.OriginalPath, modelResult.OriginalQueryString, modelResult.RequestId);
+			}
+			if (HttpContext.Features.Get<IExceptionHandlerPathFeature>() is ExceptionHandlerFeature exceptionFeature)
+			{
+				if (exceptionFeature != null)
+				{
+					modelResult.RouteOfException = exceptionFeature.Path;
+					if (exceptionFeature.Error != null)
+					{
+						modelResult.ErrorTargetSiteName = exceptionFeature.Error.TargetSite.Name;
+						modelResult.ErrorMessage = $"{exceptionFeature.Error.InnerException?.Message} | {exceptionFeature.Error?.Message}";
+						modelResult.ErrorSource = exceptionFeature.Error.Source;
+						modelResult.ErrorStackTrace = exceptionFeature.Error.StackTrace;
+						modelResult.ErrorData = JsonConvert.SerializeObject(exceptionFeature.Error.Data);
+					}
+				}
+			}
+			return View(viewName: "Error", model: modelResult);
 		}
 	}
 }
--- src/Csuf.WebApplTemplate.MvcClientApp/Models/ErrorViewModel.cs
+++ src/Csuf.WebApplTemplate.MvcClientApp/Models/ErrorViewModel.cs
@@ -1,11 +1,45 @@
-using System;
-
-namespace Csuf.WebApplTemplate.Models
+namespace Csuf.WebApplTemplate.MvcClientApp.Models
 {
 	public class ErrorViewModel
 	{
+		public string RouteOfException { get; set; }
+
+		public string ErrorMessage { get; set; }
+
+		public string ErrorSource { get; set; }
+
+		public string ErrorTargetSiteName { get; set; }
+
+		public string ErrorStackTrace { get; set; }
+
+		public string ErrorData { get; set; }
+
+		public string OriginalPath { get; set; }
+
+		public string OriginalPathBase { get; set; }
+
+		public string OriginalQueryString { get; set; }
+
 		public string RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+		public bool ShowRouteOfException => !string.IsNullOrWhiteSpace(RouteOfException);
+
+		public bool ShowErrorMessage => !string.IsNullOrWhiteSpace(ErrorMessage);
+
+		public bool ShowErrorSource => !string.IsNullOrWhiteSpace(ErrorSource);
+
+		public bool ShowErrorTargetSiteName => !string.IsNullOrWhiteSpace(ErrorTargetSiteName);
+
+		public bool ShowErrorStackTrace => !string.IsNullOrWhiteSpace(ErrorStackTrace);
+
+		public bool ShowOriginalPath => !string.IsNullOrWhiteSpace(OriginalPath);
+
+		public bool ShowOriginalPathBase => !string.IsNullOrWhiteSpace(OriginalPathBase);
+
+		public bool ShowOriginalQueryString => !string.IsNullOrWhiteSpace(OriginalQueryString);
+
+		public bool ShowRequestId => !string.IsNullOrWhiteSpace(RequestId);
+
+		public bool ShowErrorData => !string.IsNullOrWhiteSpace(ErrorData);
 	}
 }
